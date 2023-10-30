c06640bc917378c9244599a755c461aff250aace
Happy birthday to sus <117884839+Nimono-sleep-well@users.noreply.github.com>
2023-10-30 23:01:26 +0900

    【祝】ロスターコマンド実装

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ import json
 
 TOKEN = config.VCTUDB_TOKEN
 
+MYID = config.MY_ID
+
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
@@ -59,52 +61,62 @@ async def on_raw_reaction_add(payload):
 @app_commands.rename(title="大会名", team1="チーム1", team2="チーム2")
 
 async def addMatch_command(interaction: discord.Interaction, title: str, month: int, day: int, hour: int, minutes: int, team1: str, team2: str):
-   
-    MakeImage.MkImage(title, month, day, hour, minutes, team1, team2)
-    
-    file = discord.File("../out/match_Image.jpg", filename = "match_Image.jpg")
-    embed=discord.Embed(title="Image", color=0xff0000)
-    embed.set_image(url="attachment://match_Image.jpg")
-    
-    await interaction.channel.send(file=file)
-    #await interaction.response.send_message(file="../out/match_Image.jpg")
-    
-    
+    if interaction.user.id == MYID:
+        MakeImage.MkImage(title, month, day, hour, minutes, team1, team2)
+        
+        file = discord.File("../out/match_Image.jpg", filename = "match_Image.jpg")
+        embed=discord.Embed(title="Image", color=0xff0000)
+        embed.set_image(url="attachment://match_Image.jpg")
+        
+        await interaction.channel.send(file=file)
+        #await interaction.response.send_message(file="../out/match_Image.jpg")
+    else:
+        await interaction.response.send_message("管理者権限がないため，このコマンドを実行できません", ephemeral=True)
+        
     
 @tree.command(name="vote_img", description="勝利するチームを予想しよう！")
 
 async def vote_command(interaction: discord.Interaction, color: str, metatitle: str, team1: str, team2: str):
     
-    T1, T2 = TeamCheck.TeamCheck(team1, team2)
-    
-    MakeImage.MkVote(color, team1, team2)
-    file = discord.File("../out/vote_Image.jpg", filename = "vote_Image.jpg")
-    Color = TeamCheck.color(color)
-    channel = client.get_channel(channel_vote)
-    Event_title = str(metatitle) + "勝利予想！"
-    embed = discord.Embed(title= Event_title, description=str(metatitle) + "の勝利チームを予想しよう！", color=Color)
-    embed.add_field(name="投票方法", value=str(T1[2]) + "は :one: \n" + str(T2[2] + "は :two: \nのリアクションで投票！"))
-    embed.set_image(url="attachment://vote_Image.jpg")
+    if interaction.user.id == MYID:
+        T1, T2 = TeamCheck.TeamCheck(team1, team2)
+        
+        MakeImage.MkVote(color, team1, team2)
+        file = discord.File("../out/vote_Image.jpg", filename = "vote_Image.jpg")
+        Color = TeamCheck.color(color)
+        channel = client.get_channel(channel_vote)
+        Event_title = str(metatitle) + "勝利予想！"
+        embed = discord.Embed(title= Event_title, description=str(metatitle) + "の勝利チームを予想しよう！", color=Color)
+        embed.add_field(name="投票方法", value=str(T1[2]) + "は :one: \n" + str(T2[2] + "は :two: \nのリアクションで投票！"))
+        embed.set_image(url="attachment://vote_Image.jpg")
 
-    msg = await channel.send(file=file, embed=embed)
-    await msg.add_reaction(one_emoji)
-    await msg.add_reaction(two_emoji)
+        msg = await channel.send(file=file, embed=embed)
+        await msg.add_reaction(one_emoji)
+        await msg.add_reaction(two_emoji)
+    else:
+        await interaction.response.send_message("管理者権限がないため，このコマンドを実行できません", ephemeral=True)
     
 @tree.command(name="roaster", description="ロスター表示")
-
 async def roaster_command(interaction: discord.Interaction, team: str):
+    TEAM = TeamCheck.RoasterCheck(team)
     with open(file_roaster) as f:
         roaster = json.load(f)
         
         info = roaster[team]
         
+        print(info)
+        
         player = "\n".join(info["player"])
         coach = "\n".join(info["coach"])
         analyst = "\n".join(info["analyst"])
-        embed = discord.Embed(title = "ロスター情報", description = roaster[2] + "のロスター情報です")
+        
+        file = discord.File(TEAM[1], filename = "team_logo.png")
+        
+        embed = discord.Embed(title = "ロスター情報", description = TEAM[2] + "のロスター情報です", color=TEAM[3])
         embed.add_field(name="Player:", value=player)
         embed.add_field(name="Coach:", value=coach)
         embed.add_field(name="Analyst:", value=analyst)
-    await interaction.channel.send(embed=embed)
+        embed.set_thumbnail(url="attachment://team_logo.png")
+    await interaction.channel.send(file=file, embed=embed)
     
 client.run(TOKEN)
